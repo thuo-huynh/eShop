d41e1e53da618520f603e4d64ae97dbf1b22c342
HuynhNgocThuong <huynhngocthuong0208@gmail.com>
2025-04-02 11:55:43 +0700

    add: empty cart

--- src/app/(client)/cart/page.tsx
+++ src/app/(client)/cart/page.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+'use client';
+
+import EmptyCard from '@/components/cart/empty-card';
+import NoAccessToCart from '@/components/cart/no-access-to-cart';
+import Container from '@/components/layout/container';
+import useCartStore from '@/stores/cart.store';
+import { useAuth } from '@clerk/nextjs';
+import React from 'react';
 
 type Props = {};
 
 export default function Page({}: Props) {
-  return <div>Page</div>;
+  const { isSignedIn } = useAuth();
+  const {
+    deleteCartProduct,
+    getTotalPrice,
+    getItemCount,
+    getSubtotalPrice,
+    resetCart,
+    getGroupedItems,
+  } = useCartStore();
+  const cartProducts = [];
+  return (
+    <div className="bg-gray-50 pb-52 md:pb-10">
+      {isSignedIn ? (
+        <Container>{cartProducts.length > 0 ? <div></div> : <EmptyCard />}</Container>
+      ) : (
+        <NoAccessToCart />
+      )}
+    </div>
+  );
 }
